5989f5bfd05f16629e4a43000b57d0573e8e44ec
Kyle <kylegranthousel@gmail.com>
2025-09-15 12:09:04 -0400

    Complete Extending Interfaces

--- src/section2_extending_interfaces.ts
+++ src/section2_extending_interfaces.ts
@@ -8,3 +8,19 @@
 // Finally, create a variable employee1 of type Employee.
 
 // Your code here 👇
+interface Person {
+  name: string,
+  age: number,
+}
+
+interface Employee extends Person {
+  jobTitle: string,
+  department: string,
+}
+
+const employee1: Employee = {
+  name: "Kyle",
+  age: 2,
+  jobTitle: "Student",
+  department: "BT"
+}
