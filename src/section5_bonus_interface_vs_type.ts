6fa96abd3c1628383fabe56230cb1612fe488fb2
Kyle <kylegranthousel@gmail.com>
2025-09-15 12:09:31 -0400

    Complete bonus

--- src/section5_bonus_interface_vs_type.ts
+++ src/section5_bonus_interface_vs_type.ts
@@ -4,3 +4,13 @@
 // Add a comment explaining when you might use one over the other.
 
 // Your code here 👇
+interface ShapeInterface {
+  width: number,
+  height: number,
+}
+
+type ShapeType = {width: number, height: number}
+
+const shapeA: ShapeInterface = {width: 2, height: 4}
+
+const shapeB: ShapeType = {width: 4, height: 10}
