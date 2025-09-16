d376ab7489d90dc175b5ba52f6dbdf9b1e8d5130
Jen Kelly <jen.kelly@powerhrg.com>
2025-09-16 13:25:10 -0400

    Define interface Book and create myBook variable

--- src/section1_interfaces.ts
+++ src/section1_interfaces.ts
@@ -6,3 +6,15 @@
 // Then, create a variable called myBook of type Book and assign it a valid object.
 
 // Your code here 👇
+
+interface Book {
+  title: string
+  author: string
+  pages: number
+}
+
+let myBook: Book = {
+  title: "Way of Kings",
+  author: "Brandon Sanderson",
+  pages: 1000,
+}
